a9e6ee61a968348845255c5cc7da08b3b5f97e1d
RiteshTalwekar7 <ritesh0726@gmail.com>
2026-06-01 09:09:13 +0000

    Completed the notes app functionality and learned about rendering collections , onChange property , forms and how to access the value of an dom element in event.target.value

--- part2/notes-app/src/main.jsx
+++ part2/notes-app/src/main.jsx
@@ -1,6 +1,25 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "The browser can execute only javascript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of http protocol",
+    important: true
+  }
+];
+
+
 createRoot(document.getElementById('root')).render(
-  <App />
+  <App notes={notes} />
 )
