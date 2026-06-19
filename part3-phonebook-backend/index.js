6f3fbb31793164e6d8d8338de6c6b3849289f04d
RiteshTalwekar7 <ritesh0726@gmail.com>
2026-06-19 03:25:58 +0000

    completed backend configuration and api testing for deployment through render

--- part3-phonebook-backend/index.js
+++ part3-phonebook-backend/index.js
@@ -1,5 +1,6 @@
 import express, { json } from "express";
 import morgan from 'morgan';
+import cors from 'cors';
 const PORT = 3001;
 const app = express();
 
@@ -25,15 +26,17 @@ let phonebook = [
     "number": "39-23-6423122"
   }
 ]
+app.use(cors());
 
-morgan.token('details', (req, res) => {
-  return `{"name":"${req.body.name}","number":"${req.body.number}"}`
-})
+// morgan.token('details', (req, res) => {
+//   return `{"name":"${req.body.newObject.name}","number":"${req.body.newObject.number}"}`
+// })
 
 app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :details'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
-app.get('/api/persons', (request, response) => {
+
+app.get('/api/persons/', (request, response) => {
   response.status(200).send(phonebook);
 });
 
@@ -53,32 +56,30 @@ function generateIdInRange(min, max) {
 }
 
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body;
-  if (!body.name || !body.number) {
+app.post('/api/persons/', (request, response) => {
+  console.log(request.body);
+  const { name, number, id } = request.body;
+  if (!name || !number) {
     return response.status(401).json({
       error: 'content is missing'
     })
   }
 
-  // const recordExists = phonebook.filter(phone => phone.name === body.name);
-  // if (recordExists) {
-  //   return response.status(401).send({
-  //     error: 'name must be unique'
-  //   });
-  // }
-
-  const newRecord = {
-    id: generateIdInRange(phonebook.length, 100000),
-    name: body.name,
-    number: body.number
+  const newPerson = {
+    id: id || generateIdInRange(10, 10000),
+    name: name,
+    number: number
   };
 
-  phonebook.push(newRecord);
-  response.status(200).json({
-    phonebook,
-    success: 'The new record has been successfully added to the phonebook'
-  })
+  // // const recordExists = phonebook.filter(phone => phone.name === body.name);
+  // // if (recordExists) {
+  // //   return response.status(401).send({
+  // //     error: 'name must be unique'
+  // //   });
+  // // }
+
+  phonebook.push(newPerson);
+  response.status(200).json(newPerson);
 });
 
 app.get('/api/persons/:id', (request, response) => {
@@ -94,17 +95,14 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
   const { id } = request.params;
-  const record = phonebook.find(user => user.id === id);
+  const record = phonebook.find(phone => phone.id == id);
   if (!record) {
     return response.status(401).json({
       error: `the information of the person with id ${id} is already deleted or not found`
     })
   }
   phonebook = phonebook.filter(user => user.id !== id);
-  response.status(200).json({
-    phonebook,
-    success: `The information of the person with id ${id} is successfully removed.`
-  })
+  response.status(200).json(record)
 });
 
 const unknownEndpoint = (request, response) => {
